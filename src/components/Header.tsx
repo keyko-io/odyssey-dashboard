b0c48324dd716baf27212522e694e21861d13707
enrique <enrique@keyko.io>
2020-11-12 10:44:25 +0100

    Remove local path

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export function LogoTitle() {
     return (
       <Image
         style={{ width: 250, height: 83 }}
-        source={require('/home/eruiz/Projects/Odyssey_2020/odyssey-dashboard/banner_logo.png')}
+        source={require('../../resources/images/banner_logo.png')}
       />
     );
   }
