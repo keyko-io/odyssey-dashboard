eba441c5a24bb89825f7e0beb45facec338510e3
Jernej Pregelj <jernej.pregelj88@gmail.com>
2020-11-13 16:40:55 +0100

    camera loading

--- src/components/CameraView.web.tsx
+++ src/components/CameraView.web.tsx
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
 import jsQR from "jsqr";
 
-export class CameraView extends Component {
+interface Props {
+  navigation: any
+}
+
+export class CameraView extends Component<Props> {
   videoRef: any
   canvasRef: any
   canvasContext: any
+  interval: any
 
   constructor(props:any) {
     super(props);
@@ -21,11 +26,21 @@ export class CameraView extends Component {
           this.videoRef.current.play();
       });
     }
-    setInterval(()=>{
+    this.interval = setInterval(()=>{
       this.canvasContext.clearRect(0,0,640,480);
       this.canvasContext.drawImage(this.videoRef.current,0,0,640,480);
       const code = jsQR(this.canvasContext.getImageData(0, 0, 640, 480).data,640,480)
-      console.log(code) // send in code
+      if(code !== null){
+        clearInterval(this.interval)
+        // TODO: resolve data from url
+        const resolvedData = {
+          did: 'did:nvm:11111111111',
+          description: 'Dave',
+          state: 'Delivered',
+          destination: 'Malaga'
+        }
+        this.props.navigation.navigate('detailsItem', resolvedData)
+      }
     },1000)
   }
 
@@ -33,7 +48,7 @@ export class CameraView extends Component {
     return (
       <View style={{ flex: 1 }}>
           <video ref={this.videoRef} width="640" height="480" autoPlay></video>
-          <canvas ref={this.canvasRef} width="640" height="480" style={{width:640,height:480}}></canvas>
+          <canvas ref={this.canvasRef} width="640" height="480" style={{width:640,height:480,opacity:0}}></canvas>
       </View>
     );
   }
