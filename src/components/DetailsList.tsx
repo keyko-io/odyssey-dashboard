ba5423947aa2b59b7d9d5c320811152fa85f8460
Pedro Gutiérrez <pedrogp9693@gmail.com>
2020-11-13 12:59:29 +0100

    New style for packages list

--- src/components/DetailsList.tsx
+++ src/components/DetailsList.tsx
@@ -1,23 +1,33 @@
 import React from 'react';
-import { View } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { Subheading } from 'react-native-paper';
+
+import { Title } from '../ui';
+
+enum DeliveryState {
+  Active = 'In Transit',
+  Ok = 'Delivered',
+  Error = 'Problem Detected',
+}
 
 //Get this data calling on chain or to the metadata-api
-export const listItems = [  {
-  did: 'did:nvm:0123456789',
-  description: 'Robin',
-  state: 'In Transit',
+export const listItems = [
+  {
+    did: 'did:nvm:0123456789012345678901234567890123456789',
+    description: 'Robin delivery',
+    state: DeliveryState.Active,
   x: 13.421375,
   y: 52.492450,
-  destination: 'Berlin'
-},
-{
-  did: 'did:nvm:11111111111',
-  description: 'Dave',
-  state: 'Delivered',
+    destination: 'Berlin'
+  },
+  {
+    did: 'did:nvm:1111111111111111111111111111111111111111',
+    description: 'Dave delivery',
+    state: DeliveryState.Ok,
   x: -4.435115,
   y: 36.7197404,
-  destination: 'Malaga'
-},
+    destination: 'Malaga'
+  },
 ];
 
 interface Props {
@@ -25,22 +35,69 @@ interface Props {
 }
 
 export class DetailsList extends React.Component<Props> {
-    render() {
-      return (
-        <View>
-          <ul>
-            {listItems.map(item => (
-              <li key={item.did}>
-                <div onClick={() => this.props.navigation.navigate('detailsItem', item)}>
-                  <div>{item.did}</div>
-                  <div>{item.description}</div>
-                  <div>{item.state}</div>
-                  <div>{item.destination}</div>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </View>
-      );
+
+  cutDid(did: string) {
+    return did.replace(/^(\w+:\w+:[a-f0-9]{8}).+([a-f0-9]{8})$/i, '$1...$2')
+  }
+
+  getStateStyle(state: DeliveryState) {
+    switch (state) {
+      case DeliveryState.Ok: return styles.statusOk
+      case DeliveryState.Error: return styles.statusError
+      case DeliveryState.Active: return styles.statusActive
     }
+  }
+
+  render() {
+    return (
+      <View>
+        <Title>Your packages</Title>
+        <View>
+          {listItems.map(item => (
+            <TouchableOpacity
+              key={item.did}
+              onPress={() => this.props.navigation.navigate('detailsItem', item)}>
+
+              <View style={styles.item}>
+                <Text style={styles.textMono}>DID: {this.cutDid(item.did)}</Text>
+                <Subheading>{item.description}</Subheading>
+                <Subheading style={styles.textSubAlt}>
+                  State: {' '}
+                  <Text style={this.getStateStyle(item.state)}>
+                    {item.state}
+                  </Text>
+                </Subheading>
+              </View>
+            </TouchableOpacity>
+          ))}
+        </View>
+      </View>
+    );
+  }
 }
+
+
+const styles = StyleSheet.create({
+  item: {
+    padding: 16,
+  },
+  textMono: {
+    fontFamily: 'monospace',
+    fontSize: 14,
+    marginBottom: 4,
+  },
+  textSubAlt: {
+    color: 'color: rgba(0, 0, 0, 0.6)',
+    lineHeight: 20,
+  },
+
+  statusOk: {
+    color: '#2A9D8F',
+  },
+  statusError: {
+    color: '#FF0C3E',
+  },
+  statusActive: {
+    color: '#7C02F3',
+  },
+})
