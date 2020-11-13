0185abf21d3dabee27f1c7afe473cd9298bfdd0d
Pedro Gutiérrez <pedrogp9693@gmail.com>
2020-11-13 12:07:16 +0100

    Fix tabulation issues

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -5,20 +5,20 @@ interface Props {
   navigation: any
 }
 export class Dashboard extends React.Component<Props> {
-    render() {
-      return (
-        <View>
-          <Button title="Start" onPress={() => this.props.navigation.navigate('detailsList')}></Button>
-          <br></br>
-          <Button title="DHL" onPress={()=> this.props.navigation.navigate('detailsList')}></Button>
-          <br></br>
-          <Button title="KLM" onPress={()=> this.props.navigation.navigate('detailsList')}></Button>
-          <br></br>
-          <Button title="John Doe" onPress={()=> this.props.navigation.navigate('detailsList')}></Button>
-          <br></br>
-          <Button title="Register" onPress={()=> this.props.navigation.navigate('register')}></Button>
-        </View>
-      );
-    }
+  render() {
+    return (
+      <View>
+        <Button title="Start" onPress={() => this.props.navigation.navigate('detailsList')}></Button>
+        <br></br>
+        <Button title="DHL" onPress={()=> this.props.navigation.navigate('detailsList')}></Button>
+        <br></br>
+        <Button title="KLM" onPress={()=> this.props.navigation.navigate('detailsList')}></Button>
+        <br></br>
+        <Button title="John Doe" onPress={()=> this.props.navigation.navigate('detailsList')}></Button>
+        <br></br>
+        <Button title="Register" onPress={()=> this.props.navigation.navigate('register')}></Button>
+      </View>
+    );
+  }
 } 
 
--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -10,38 +10,38 @@ interface Props {
   navigation: any
 }
 type Inputs = {
-    name: string,
-    description: string,
-  };
+  name: string,
+  description: string,
+};
 
 
 export function Register() {
-    const { register, handleSubmit, errors } = useForm<Inputs>()
+  const { register, handleSubmit, errors } = useForm<Inputs>()
 
-    //TODO change to register using nevermined-sdk
-    const onSubmit = (data:any) => listItems.push({...data, state:'Registered', did: `did:nvm:${Math.random()*1234567890}`})
+  //TODO change to register using nevermined-sdk
+  const onSubmit = (data:any) => listItems.push({...data, state:'Registered', did: `did:nvm:${Math.random()*1234567890}`})
 
-    return (
+  return (
     <View>
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <br/> 
-            <Text>Name: </Text>
-            <input name="name" defaultValue="test" ref={register({ required: true })} />
-            <br/> 
-            {errors.name && <span>This field is required</span>}
-            <br/>
-            <Text>Description: </Text>    
-            <input name="description" ref={register} />
-            <br/>           
-            <br/>
-            <Text>Destination: </Text>
-            <input name="destination" ref={register} />
-            <br/>    
-            <br/>    
-            <input type="submit"/>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <br/> 
+        <Text>Name: </Text>
+        <input name="name" defaultValue="test" ref={register({ required: true })} />
+        <br/> 
+        {errors.name && <span>This field is required</span>}
+        <br/>
+        <Text>Description: </Text>    
+        <input name="description" ref={register} />
+        <br/>           
+        <br/>
+        <Text>Destination: </Text>
+        <input name="destination" ref={register} />
+        <br/>    
+        <br/>    
+        <input type="submit"/>
 
-        </form>
+      </form>
     </View>
-    );
-    
+  );
+  
 }
