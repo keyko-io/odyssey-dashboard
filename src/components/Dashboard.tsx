477cd745c8b42b9cc86fc34ce149cc707b8ca687
Esraa Jbara <jbesraa@gmail.com>
2020-11-14 19:57:37 +0200

    style dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import { Button, View } from 'react-native';
+import { Text, Image, View, StyleSheet, TouchableHighlight } from 'react-native';
 import { Context } from '../../context';
+import MSDLogo from '../../resources/images/msd'
+import KLMLogo from '../../resources/images/klm'
+import DHLLogo from '../../resources/images/dhl'
+import RECIPIENTLogo from "../../resources/images/recipient"
 
 interface Props {
-  navigation: any
+  navigation: any,
 }
 
 export class Dashboard extends React.Component<Props, {}> {
@@ -17,12 +21,103 @@ export class Dashboard extends React.Component<Props, {}> {
   render() {
     return (
       <View>
-        <Button title="MSD" onPress={()=> this.openList('MSD')}></Button>
-        <Button title="DHL" onPress={()=> this.openList('DHL')}></Button>
-        <Button title="KLM" onPress={()=> this.openList('KLM')}></Button>
-        <Button title="Final receipt" onPress={()=> this.openList('FR')}></Button>
+        <Text style={styles.title}>Welcome</Text>
+        <View style={styles.tilesWrapper}> 
+          <View style={styles.tile}>
+            <View style={styles.logoWrapper}>
+              <MSDLogo />
+            </View>
+            <View style={styles.textWrapper}>
+              <Text style={styles.text}>
+                MSD {"\n"} <Text style={styles.companySubtitle}>Supplier</Text>
+              </Text>
+            </View>
+          </View>
+          <TouchableHighlight onPress={()=> this.openList('DHL')}>
+            <View style={styles.tile}> 
+              <View style={styles.logoWrapper}>
+                <DHLLogo/>
+              </View>
+              <View style={styles.textWrapper}>
+                <Text style={styles.text}>
+                  DHL {"\n"} <Text style={styles.companySubtitle}>Logistics</Text>
+                </Text>
+              </View>
+            </View>
+          </TouchableHighlight>
+          <TouchableHighlight onPress={()=> this.openList('KLM')}>
+          <View style={styles.tile}> 
+            <View style={styles.logoWrapper}>
+              <KLMLogo/>
+            </View>
+            <View style={styles.textWrapper}>
+              <Text style={styles.text}>
+                KLM {"\n"} <Text style={styles.companySubtitle}>Logistics</Text>
+              </Text>
+            </View>
+          </View>
+          </TouchableHighlight>
+          <TouchableHighlight onPress={()=> this.openList('FR')}>
+          <View style={styles.tile}>
+            <View style={styles.logoWrapper}>
+              <RECIPIENTLogo/>
+            </View>
+            <View style={styles.textWrapper}>
+              <Text style={styles.text}>
+                Final {"\n"} <Text style={styles.companySubtitle}>Recpient</Text>
+              </Text>
+            </View>
+          </View>
+        </TouchableHighlight>
       </View>
+    </View>
     );
   }
 } 
 
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 28,
+    color: '#7C02F3',
+    paddingTop: '2.3em',
+    textAlign: 'center',
+  },
+  tilesWrapper: {
+    justifyContent: 'center',
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+    paddingTop: '2.5em',
+    flex: 1,
+  },
+  tile: {
+    margin: 10,
+    display: 'flex',
+    flexDirection: 'row',
+    height: 100,
+    alignItems: 'center',
+    borderColor: '#86FFEA',
+    borderWidth: 2,
+    borderRadius: 12
+  },
+  text: {
+    fontSize: 16,
+    textAlign: 'center',
+    fontWeight: '700',
+  },
+  backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover', 
+  },
+  logoWrapper: {
+    display: 'flex',
+    alignItems: 'flex-end',
+    width: '30%',
+  },
+  companySubtitle: {
+    fontWeight: '400'
+  },
+  textWrapper: {
+    width: '70%',
+    display: 'flex',
+  }
+})
